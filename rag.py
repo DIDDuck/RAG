57b9e0cc21f361025ee594d5dd7d0dd4755df220
DIDDuck <179592401+DIDDuck@users.noreply.github.com>
2025-10-19 13:16:23 +0300

    Running possible only using RAGProcessor class now.

--- rag.py
+++ rag.py
@@ -6,109 +6,28 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_ollama import OllamaEmbeddings
 from langchain_text_splitters.character import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
+from RAGProcessor import RAGProcessor
 
 load_dotenv()
 
-# Initialize documents folder
-if not os.path.exists("./documents"): os.mkdir("./documents")
+rag = RAGProcessor(file_path, db_path, llm_model, os.getenv("LLM_URL"), embeddings_model, filename_filter)
 
+# Split data and send it to vectorstore (db)
+vector_db =  rag.send_data_to_vectorstore()
 
-def split_file_to_chunks(file_path):
-    '''File loader: load file and split it to chunks'''
+# Get query from user
+query = rag.get_user_query()
 
-    if not os.path.exists(file_path):
-        print(f"File \"{file_path}\" does not exist.")
-        exit()
-    loader = PyPDFLoader(
-        file_path = file_path,
-        )
-    pages = loader.load()
+# Get most relevant documents from db
+relevant_documents = rag.retrieve_documents(vector_db, query)
 
-    # After loading documents, they need to be split into small pieces (chuncks).
-    splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 100)
-    chunks = splitter.split_documents(pages)
+# Form context to send to LLM
+context_from_documents = rag.create_context_from_documents(relevant_documents)
+context_from_notes = rag.create_context_from_notes(os.getenv("NOTES_FOR_CONTEXT"))
 
-    return chunks
+# Client for contacting Ollama server
+ollama_client = rag.create_ollama_client()
 
-
-# Create embeddings Using embedding model: nomic-embed-text. Using Langchain Ollama here.
-embeddings = OllamaEmbeddings(
-    model = embeddings_model,
-    base_url = os.getenv("LLM_URL")
-)
-
-# Send (embedded) chunks to vectorstore (ChromaDB for example). Use existing store or create new.
-if os.path.exists(db_path):
-    print("Opening existing vectorstore.") 
-    chroma_client = chromadb.PersistentClient(path = db_path)
-    vector_db = Chroma(
-        client = chroma_client,
-        collection_name = os.getenv("COLLECTION_NAME"),
-        embedding_function = embeddings,
-        create_collection_if_not_exists = True
-    )
-    # If documents from current filename don't exist, send chunks to db.
-    if len(vector_db.similarity_search(query = file_path, k = 1, filter = {"source": f"{file_path}"})) == 0:
-        print("Saving data to vectorstore, please wait...")
-        chunks = split_file_to_chunks(file_path)
-        vector_db.add_documents(documents = chunks)
-        print("Opened existing vectorstore and saved data in it.") 
-    else:   
-        print(f"Opened existing vectorstore with existing data from file: {file_path}")
-else:   
-    print("Creating new vectorstore, please wait... ")
-    chunks = split_file_to_chunks(file_path)
-    os.mkdir(db_path) 
-    vector_db = Chroma.from_documents(
-        documents = chunks,
-        collection_name = os.getenv("COLLECTION_NAME"), 
-        embedding = embeddings,
-        persist_directory = db_path
-    )
-    print("Created new vectorstore and saved data in it.")
-
-
-# When user has a question, it is sent for retrieval from vectorstore.
-query = input("\nGive me a question for RAG App: ")
-
-# Question/query also needs to be converted to embedded form so that it can searched in vectorstore. Using retriever for this.
-retriever = vector_db.as_retriever(search_kwargs = {"k": 10})
-retrieved_documents = retriever.invoke(input = query, filter = None if not filename_filter else {"source": f"{file_path}"})
-
-#for document in retrieved_documents:
-#    print("Source:", document.metadata["source"])
-#print(f"NUMBER of documents: {len(retrieved_documents)}")
-
-# Setting context for sending query to LLM: relevant documents from Chromadb and some specific notes.
-documents_for_context = "\n\n".join([document.page_content for document in retrieved_documents]) # The most informative documents' content retrieved from vector store.
-notes_for_context = os.getenv("NOTES_FOR_CONTEXT")
-
-# For custom url, client is needed
-ollama_client = ollama.Client(
-    host = os.getenv("LLM_URL")
-)
-
-# Send context and query to LLM
-print("Sending query for LLM. Please wait for an answer...\n")
-
-response_chat = ollama_client.chat(
-    model = llm_model,
-    messages = [
-        {
-            "role": "system",
-            "content": notes_for_context + documents_for_context + "Use ONLY this information and base your answer on it."
-        },
-        {
-            "role": "user",
-            "content": query
-        } 
-    ],
-    stream = True
-)
-
-
-# Streaming chat response
-for part in response_chat:
-    print(part["message"]["content"], end = "")
-print("\n")
-
+# Get response
+response = rag.send_query(ollama_client, context_from_documents, context_from_notes, query)
+rag.show_streaming_response(response)
